8b2ef02731b0ec8be68a9c22059cc9560df8a100
Yang Jeong Hun <onyxclover9931@gmail.com>
2023-12-31 21:17:50 +0900

    j5-common: Add j5ltekx
     * j5ltekx: SM-J500N0
    
    Signed-off-by: Yang Jeong Hun <onyxclover9931@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter j5lte j5ltechn j5nlte j5nltexx j53gxx, $(TARGET_DEVICE)),)
+ifneq ($(filter j5lte j5ltechn j5ltekx j5nlte j5nltexx j53gxx, $(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
